59d68b777d96457443a9e081c1eaea71a38994a7
Andrade <luisfelipeandrade@gmail.com>
2024-10-24 17:36:02 -0300

    mock services rest

--- mock_service.py
+++ mock_service.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -15,4 +15,4 @@ def cyberark_chave():
     return jsonify({"message": "Mocked CyberArk chave"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
